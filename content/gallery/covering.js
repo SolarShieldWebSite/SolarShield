313beb57dd0d4960063bce2f853efd02093b07d5
arthurmtro <arthurmtro@gmail.com>
2023-01-06 16:02:02 +0100

    fix links

--- content/gallery/covering.js
+++ content/gallery/covering.js
@@ -1,14 +1,14 @@
 export const coveringData = [
   {
-    src: "/images/covering/C_1.JPG",
+    src: "/images/covering/C_1.jpg",
     alt: "Covering_1",
   },
   {
-    src: "/images/covering/C_2.JPG",
+    src: "/images/covering/C_2.jpg",
     alt: "Covering_2",
   },
   {
-    src: "/images/covering/C_3.JPG",
+    src: "/images/covering/C_3.jpg",
     alt: "Covering_3",
   },
   {
@@ -16,103 +16,103 @@ export const coveringData = [
     alt: "Covering_4",
   },
   {
-    src: "/images/covering/C_5.JPG",
+    src: "/images/covering/C_5.jpg",
     alt: "Covering_5",
   },
   {
-    src: "/images/covering/C_6.JPG",
+    src: "/images/covering/C_6.jpg",
     alt: "Covering_6",
   },
   {
-    src: "/images/covering/C_7.JPG",
+    src: "/images/covering/C_7.jpg",
     alt: "Covering_7",
   },
   {
-    src: "/images/covering/C_8.JPG",
+    src: "/images/covering/C_8.jpg",
     alt: "Covering_8",
   },
   {
-    src: "/images/covering/C_9.JPG",
+    src: "/images/covering/C_9.jpg",
     alt: "Covering_9",
   },
   {
-    src: "/images/covering/C_10.JPG",
+    src: "/images/covering/C_10.jpg",
     alt: "Covering_10",
   },
   {
-    src: "/images/covering/C_11.JPG",
+    src: "/images/covering/C_11.jpg",
     alt: "Covering_11",
   },
   {
-    src: "/images/covering/C_12.JPG",
+    src: "/images/covering/C_12.jpg",
     alt: "Covering_12",
   },
   {
-    src: "/images/covering/C_13.JPG",
+    src: "/images/covering/C_13.jpg",
     alt: "Covering_13",
   },
   {
-    src: "/images/covering/C_14.JPG",
+    src: "/images/covering/C_14.jpg",
     alt: "Covering_14",
   },
   {
-    src: "/images/covering/C_15.JPG",
+    src: "/images/covering/C_15.jpg",
     alt: "Covering_15",
   },
   {
-    src: "/images/covering/C_16.JPG",
+    src: "/images/covering/C_16.jpg",
     alt: "Covering_16",
   },
   {
-    src: "/images/covering/C_17.JPG",
+    src: "/images/covering/C_17.jpg",
     alt: "Covering_17",
   },
   {
-    src: "/images/covering/C_18.JPG",
+    src: "/images/covering/C_18.jpg",
     alt: "Covering_18",
   },
   {
-    src: "/images/covering/C_19.JPG",
+    src: "/images/covering/C_19.jpg",
     alt: "Covering_19",
   },
   {
-    src: "/images/covering/C_20.JPG",
+    src: "/images/covering/C_20.jpg",
     alt: "Covering_20",
   },
   {
-    src: "/images/covering/C_21.JPG",
+    src: "/images/covering/C_21.jpg",
     alt: "Covering_21",
   },
   {
-    src: "/images/covering/C_22.JPG",
+    src: "/images/covering/C_22.jpg",
     alt: "Covering_22",
   },
   {
-    src: "/images/covering/C_23.JPG",
+    src: "/images/covering/C_23.jpg",
     alt: "Covering_23",
   },
   {
-    src: "/images/covering/C_24.JPG",
+    src: "/images/covering/C_24.jpg",
     alt: "Covering_24",
   },
   {
-    src: "/images/covering/C_25.JPG",
+    src: "/images/covering/C_25.jpg",
     alt: "Covering_25",
   },
   {
-    src: "/images/covering/C_26.JPG",
+    src: "/images/covering/C_26.jpg",
     alt: "Covering_26",
   },
   {
-    src: "/images/covering/C_27.JPG",
+    src: "/images/covering/C_27.jpg",
     alt: "Covering_27",
   },
   {
-    src: "/images/covering/C_28.JPG",
+    src: "/images/covering/C_28.jpg",
     alt: "Covering_28",
   },
   {
-    src: "/images/covering/C_29.JPG",
+    src: "/images/covering/C_29.jpg",
     alt: "Covering_29",
   },
 ];
